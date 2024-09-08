044909e086ab625d1472026d79772feb124d37c4
saliltrehan <cdusms@gmail.com>
2024-09-08 19:59:10 +0530

    test update

--- test.py
+++ test.py
@@ -4,10 +4,7 @@ from src.chat_service.RAG.utils.constants import OPENAI_COMPATIBLE_API_MODEL_NAM
 print(OPENAI_COMPATIBLE_API_MODEL_NAME)
 
 llm = CustomOpenAI(
-    model=OPENAI_COMPATIBLE_API_MODEL_NAME,
     max_tokens=1000,
-    api_key=OPENAI_COMPATIBLE_API_KEY,
-    base_url=OPENAI_COMPATIBLE_API_BASE+'/v1'
 )
 
-llm.invoke("Write a 7-day itenary for Singapore")
+print(llm.invoke("Write a 7-day itenary for Singapore"))
