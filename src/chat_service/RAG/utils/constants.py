40fd333b687087177f7acfd3dbbc5a2fad55d795
saliltrehan <cdusms@gmail.com>
2024-11-23 19:45:57 +0530

    Adding some env variables

--- src/chat_service/RAG/utils/constants.py
+++ src/chat_service/RAG/utils/constants.py
@@ -61,12 +61,14 @@ def generate_system_message(lang: str) -> str:
 
 _default_system_message: str =  generate_system_message("English")
 
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
+
 EMBEDDING_API_MODEL_URL=os.getenv("MODEL_API_URL_EMBEDDING", "")
 EMBEDDING_API_MODEL_KEY=os.getenv("MODEL_API_KEY_EMBEDDING", "EMPTY")
 EMBEDDING_API_MODEL_NAME=os.getenv("MODEL_API_NAME_EMBEDDING","bge-m3")
 
 OPENAI_COMPATIBLE_API_KEY = os.getenv("MODEL_API_KEY_CHAT","")
-OPENAI_COMPATIBLE_API_BASE = os.getenv("MODEL_API_URL_CHAT","")
+OPENAI_COMPATIBLE_API_BASE = os.getenv("MODEL_API_URL_CHAT","EMPTY")
 OPENAI_COMPATIBLE_API_MODEL_NAME = os.getenv("MODEL_API_NAME_CHAT","")
 
 # Arabic
